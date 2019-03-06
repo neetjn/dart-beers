5a2d0e51d863b1c095d33c404da97d090200a423
John Nolette <jnolette@transparent.com>
2019-03-06 16:11:45 -0500

    Completing beer entity model

--- beers/lib/models/beer.dart
+++ beers/lib/models/beer.dart
@@ -1,11 +1,31 @@
 library beers.lib.constants;
 
-import 'package:aqueduct';
+import 'package:aqueduct/aqueduct.dart';
 
-// class Beer extends ManagedObject<_Beer> implements Beer{}
+class Beer extends ManagedObject<_Beer> implements _Beer{}
 
-// class _Beer {
-//   @ManagedPrimaryKey
-//   String name;
-//   String
-// }
+// essentially a DAO?
+// add documentation for what's going on with the -
+// @ symbol/decorator
+class _Beer {
+  @primaryKey
+  String id;
+
+  @Column(unique: true, indexed: true)
+  String name;
+
+  @Column(indexed: true)
+  String brewery;
+
+  @Column()
+  String style;
+
+  @Column(indexed: true)
+  double alcohol;
+
+  @Column()
+  int ibu;
+
+  @Column()
+  int srm;
+}
