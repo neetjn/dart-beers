8aaf8458e16c3b08ce332dda56dc1c413d5a2211
John Nolette <jnolette@transparent.com>
2019-03-08 16:08:47 -0500

    Working on http practice

--- practice/http.dart
+++ practice/http.dart
@@ -1,14 +1,40 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
+class SwapiRelMap {
+  static final String People = 'people';
+  static final String Planets = 'planets';
+  static final String Films = 'films';
+  static final String Species = 'species';
+  static final String Vehicles = 'vehicles';
+  static final String Starships = 'starships';
+}
+
+final Uri SwapiBaseUri = Uri.parse('https://swapi.co/api/');
+Uri peopleUri;
+
 void main() {
+
   HttpClient()
-    // produces a request object
-    .getUrl(Uri.parse('https://swapi.co/api/people/1'))
-    // sends the request
+    .getUrl(SwapiBaseUri)
     .then((request) => request.close())
-    // transforms and prints the response
     .then((response) {
-      response.transform(Utf8Decoder()).listen(print);
+      response.transform(Utf8Decoder()).listen((body) {
+        var apiRoot = json.decode(body);
+        peopleUri = Uri.parse(apiRoot[SwapiRelMap.People]);
+        print(peopleUri);
+      });
     });
+
+
+  // HttpClient()
+  //   // produces a request object
+  //   .getUrl(Uri.parse('https://swapi.co/api/people/1'))
+  //   // sends the request
+  //   .then((request) => request.close())
+  //   // transforms and prints the response
+  //   .then((response) {
+  //     response.transform(Utf8Decoder()).listen(print);
+  //   });
 }
