3d37f286c65ceae92f55ee49ec393b79d97e3964
John Nolette <jnolette@transparent.com>
2019-03-08 14:59:41 -0500

    Adding list and http practice

---
+++ practice/arrays.dart
@@ -0,0 +1,8 @@
+void main() {
+  const range = 5;
+  List<dynamic> numbers = Iterable.generate(range).toList();
+  var empty = Iterable.empty();
+  List<dynamic> _empty = Iterable.empty().toList();
+
+  List<String> dictionary = ['Foo', 'Bar', 'lol'];
+}
