b79fe5e3d49acf202303a2572ded27b63af44d83
John Nolette <jnolette@transparent.com>
2019-03-13 14:29:22 -0400

    Adding practice w/ directories

--- practice/io.dart
+++ practice/io.dart
@@ -1,13 +1,27 @@
 import 'dart:io';
 
 void main() async {
-  final file = new File('io.txt');
+  // create new directory
+  final dir = new Directory('test');
+  assert(!dir.existsSync());
+  dir.createSync();
+  assert(dir.existsSync());
+  print(dir.path);
+  // create new file
+  final file = new File('test/io.txt');
+  assert(!file.existsSync());
+  file.createSync();
+  assert(file.existsSync());
   file.writeAsStringSync('Hello World');
   String v = file.readAsStringSync();
   print(v);
   await file.writeAsString(v + '\nHello John').then((file) {
     file.readAsString().then((contents) => print(contents));
   });
+  // delete file
   file.deleteSync();
   assert(!file.existsSync());
+  // delete directory
+  dir.deleteSync();
+  assert(!dir.existsSync());
 }
