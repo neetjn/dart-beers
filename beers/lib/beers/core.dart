a5a6333abfed7f54fd8d1ebb2a19736ffb728a39
John Nolette <jnolette@transparent.com>
2019-03-19 14:23:09 -0400

    Adding bootstrapping

--- beers/lib/beers/core.dart
+++ beers/lib/beers/core.dart
@@ -5,10 +5,28 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-void bootstrapBeers() async {
-  // add logic for wiping database
+import 'package:aqueduct/aqueduct.dart';
+
+import 'package:beers/models/beer.dart';
+
+void bootstrapBeers(ManagedContext context) async {
+  // delete existing beers from database
+  // context.persistentStore.deleteTable(table)
+  // create new table
+  // context.persistentStore.createTable(Beer);
   File file = new File('../../../data/beers.json');
   String raw = file.readAsStringSync();
   List<Map> data = json.decode(raw);
-  // process data into models and store in db
+  data.forEach((Map beerData) {
+    Beer beer = Beer()
+      ..name = beerData['name']
+      ..brewery = beerData['brewery']
+      ..style = beerData['style']
+      ..alcohol = beerData['alcohol'] as double
+      ..ibu = beerData['ibu'] as int
+      ..srm = beerData['srm'] as int;
+    final Query query = Query<Beer>(context)
+      ..values = beer;
+    query.insert();
+  });
 }
